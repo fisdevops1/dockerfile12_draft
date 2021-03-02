4299f61071ce3647dc0208e5fc92c83f3bb814b4
fisdevops1 <78811413+fisdevops1@users.noreply.github.com>
2021-03-02 12:15:05 -0600

    Add files via upload

---
+++ InstantWithdrawal/instantWithdrawal.java
@@ -0,0 +1,5 @@
+public class instantWithdrawal{
+    public static void main(String args[]) {
+        System.out.println("Alert! The Amount " + args[0] + " dollars is withdrawn from your account.Check your Balance.");
+    }
+}
