c5793ee324463605b29919f8d1a61726252c67aa
fisdevops1 <78811413+fisdevops1@users.noreply.github.com>
2021-03-02 12:13:37 -0600

    Add files via upload

---
+++ DirectDeposit/directDeposit.java
@@ -0,0 +1,5 @@
+public class directDeposit{
+    public static void main(String args[]) {
+        System.out.println("The Amount " + args[0] + " dollars is direct deposited to your account. confirmed");
+    }
+}
