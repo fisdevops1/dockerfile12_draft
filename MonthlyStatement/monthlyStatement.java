78a5a994c2ecb80f6242bdbdc6c234883caed97b
fisdevops1 <78811413+fisdevops1@users.noreply.github.com>
2021-03-02 12:16:43 -0600

    Add files via upload

---
+++ MonthlyStatement/monthlyStatement.java
@@ -0,0 +1,5 @@
+public class monthlyStatement{
+    public static void main(String args[]) {
+        System.out.println("Your Monthly Statement Report is sent first week of the month including transactions upto previous month.Thanks!");
+    }
+}
